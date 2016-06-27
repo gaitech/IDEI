d8886a5afe5ddd7569023a90d0537ba5dd1263be
Keshav <keshav.k@gai.co.in>
2016-06-27 12:57:13 +0530

    fix sedcondry menu for mobile

--- sites/all/themes/idei/templates/pages/node--impact_inner_pages.tpl.php
+++ sites/all/themes/idei/templates/pages/node--impact_inner_pages.tpl.php
@@ -2,7 +2,6 @@
 
 $page_title = $node->field_graph_sub_title['und'][0]['value'];
 $header_image = $node->field_impact_header_image['und'][0]['uri'];
-$header_slogan = $node->field_header_label['und'][0]['value'];
 $header_style = 'overview_header';
 $header_image_path = image_style_url($header_style, $header_image);
 
@@ -10,7 +9,6 @@ $header_image_path = image_style_url($header_style, $header_image);
 <div class="economic-inner-main-wrapper">
 	<div class="header-wrap">
 	  <div class="header-image"> <img src="<?php echo $header_image_path; ?>"> </div>
-	  <div class="header-title"><?php echo $header_slogan; ?></div>
 	</div>
 	<div class="title-body-wrapper">
 	  <div class="economic-inner-title"><?php echo $page_title; ?></div>
