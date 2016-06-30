cdb986c72c090d620b10acb82379ca7d4984d7d0
Keshav <keshav.k@gai.co.in>
2016-06-30 11:10:18 +0530

    adding maenu in program page

--- sites/all/modules/custom/idei_custom/templates/idei-programs-menu.tpl.php
+++ sites/all/modules/custom/idei_custom/templates/idei-programs-menu.tpl.php
@@ -1,12 +1,18 @@
 <ul class="program-menu-navigation">
-<?php $menuItems = menu_tree_all_data('menu-programs');
+<?php 
+global $base_url;
+$menuItems = menu_tree_all_data('menu-programs');
 
+print '<li><a href="'.$base_url.'/#home-block-block-4">PROGRAMS : </a></li>';
 $i=1;
 foreach($menuItems as $child) {
    $title  = $child['link']['link_title'];
     print '<li><a href="#program-'.$i.'" rel="" id="anchor1" class="anchorLink">'.$title.'</a></li>';
     $i++;
     }
+   print '<li><a href="#program-6" rel="" id="anchor1" class="anchorLink"> RESEARCH AND DEVELOPMENT </a></li>';
+   print '<li><a href="#program-7" rel="" id="anchor1" class="anchorLink">PAST PROJECTS</a></li>';
+    
 echo "</ul>"; 
 ?>
 
