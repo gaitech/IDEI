42cf012a6a05976f86cf209e23d7a0990bf5c144
Keshav <keshav.k@gai.co.in>
2016-07-12 16:42:08 +0530

    create content type for approach

---
+++ sites/all/themes/idei/templates/pages/node--approach_inner_pages.tpl.php
@@ -0,0 +1,29 @@
+<?php
+
+$page_title = $node->title;
+
+$header_image = $node->field_approach_header_image['und'][0]['uri'];
+$header_style = 'overview_header';
+$header_image_path = image_style_url($header_style, $header_image);
+
+$header_image_caption = $node->field_approach_header_image_capt['und'][0]['value'];
+$page_body = $node->body['und'][0]['value'];
+
+$body_image = $node->field_body_image['und'][0]['uri'];
+$body_style = 'program_image_410___320_';
+$body_image_path = image_style_url($body_style,$body_image);
+
+?>
+<div class="economic-inner-main-wrapper">
+	<div class="detail-header-image"><img src="<?php echo $header_image_path; ?>">
+     <div class="haeder-caption"><?php print $header_image_caption; ?></div>
+    </div>
+
+	<div class="title-body-wrapper">
+	  <div class="economic-inner-title approach-title"><?php echo $page_title; ?></div>
+	  <div class="economic-inner-content approach-content">
+	  	<span class="shift-image"><img src="<?php echo $body_image_path; ?>"></span>
+	  	<?php print $page_body; ?></div>
+	</div>
+
+</div>
