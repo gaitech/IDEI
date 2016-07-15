0a1fd45b45e48a5b0d2c6f3fb6274edc84f95957
Keshav <keshav.k@gai.co.in>
2016-07-15 13:05:30 +0530

    Update program menu from code

--- sites/all/modules/custom/idei_custom/templates/idei-programs-menu.tpl.php
+++ sites/all/modules/custom/idei_custom/templates/idei-programs-menu.tpl.php
@@ -10,7 +10,6 @@ foreach($menuItems as $child) {
     print '<li><a href="#program-'.$i.'" rel="" id="anchor1" class="anchorLink">'.$title.'</a></li>';
     $i++;
     }
-   print '<li><a href="#program-6" rel="" id="anchor1" class="anchorLink"> RESEARCH AND DEVELOPMENT </a></li>';
    print '<li><a href="#program-7" rel="" id="anchor1" class="anchorLink">PAST PROJECTS</a></li>';
     
 echo "</ul>"; 
