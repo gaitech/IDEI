2db40203a6fc5fcc4611ab2d235474f58a750aa1
Keshav <keshav.k@gai.co.in>
2016-07-27 14:31:37 +0530

    create about us pages

---
+++ sites/all/themes/idei/templates/pages/node--board_members.tpl.php
@@ -0,0 +1,20 @@
+<?php
+$title = $node->title;
+
+$image = $node->field_member_image['und'][0]['uri'];
+$image_style = 'profile_picture';
+$image_path = image_style_url($image_style,$image);
+$designation = $node->field_member_designation['und'][0]['value'];
+$body = $node->body['und'][0]['value'];
+$image = '<img src="'.$image_path.'">';
+?>
+<div class="banner-image">
+<?php print views_embed_view('header_banner','block_5'); ?>
+</div>
+
+<div class="pic-title"><?php print $title; ?></div>
+<div class="pic-designation"><?php print $designation; ?></div>
+<div class="pic-main-wrap">
+  <div class="pic-picture"><?php print $image; ?></div>
+  <div class="pic-description"><?php print $body; ?></div>
+</div>
