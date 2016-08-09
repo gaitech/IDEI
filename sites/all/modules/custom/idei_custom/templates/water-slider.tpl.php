ee440a75fde8945feff84b47330e3f6333186a0c
Keshav <keshav.k@gai.co.in>
2016-08-09 17:37:28 +0530

    create feature for water section

---
+++ sites/all/modules/custom/idei_custom/templates/water-slider.tpl.php
@@ -0,0 +1,73 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+ <?php global $base_url; 
+ print '<script src="'.$base_url.'/sites/all/modules/custom/idei_custom/js/jquery.waterwheelCarousel.js"></script>';?>
+ <script type="text/javascript">
+      $(document).ready(function () {
+        jQuery(".3d-content-wrap .3dd").hide();
+        jQuery(".3d-content-wrap .3dd.3d-item-0").show();
+          jQuery('.3d-check a').click(function() {
+            var a =jQuery(this).attr('class');
+            jQuery(".3d-content-wrap .3dd").fadeOut();
+            jQuery(".3d-content-wrap ."+a).fadeIn();
+          });
+
+        var carousel = $("#carousel").waterwheelCarousel({
+          flankingItems: 3,
+          movingToCenter: function ($item) {
+            $('#callback-output').append('test');
+          },
+        });
+        $('#reload').bind('click', function () {
+          newOptions = eval("(" + $('#newoptions').val() + ")");
+          carousel.reload(newOptions);
+          return false;
+        });
+
+      });
+    </script>
+
+    <style type="text/css">
+      #carousel {
+         height:300px;
+        position:relative;
+        clear:both;
+        overflow:hidden;
+      }
+      #carousel img {
+        visibility:hidden; /* hide images until carousel can handle them */
+        cursor:pointer; /* otherwise it's not as obvious items can be clicked */
+      }
+    </style>
+<?php
+print '<div id="carousel" class="3d-check">';
+  $result = db_select('node', 'n') 
+    ->fields('n',array('nid'))  
+     ->condition('n.type', 'water_3d_gallery')
+    ->execute() 
+    ->fetchAll();
+    $count = count($result);
+    for($i=0; $i<=$count; $i++) {
+    $nid = $result[$i]->nid;
+    $node = node_load($nid);
+    $image = $node->field_water_image['und'][0]['uri'];
+
+    $body_style = 'program_image_410___320_';
+    $body_image_path = image_style_url($body_style,$image);
+
+    print '<a href="#" class="3d-item-'.$i.'"><img src="'.$body_image_path.'" id="item-'.$i.'"  /></a>';
+    }
+    print '</div>';
+    print '<div class="3d-content-wrap">';
+    for($i=0; $i<=$count; $i++) {
+    $nid1 = $result[$i]->nid;
+    $node1 = node_load($nid1);
+    $title = $node1->title;
+    $description = $node1->body['und'][0]['value'];
+    $text = $node1->field_global_water_foot_print_li['und'][0]['value'];
+    print '<div class="3dd 3d-item-'.$i.'">
+    <div class="3d-title">'.$title.'</div><div class="measure">'.$text.'</div><div class="3d-description">'.$description.'</div>
+    </div>';
+    }
+    print "</div>";
+?> 
+
---
+++ sites/all/themes/idei/templates/pages/node--water-inner-pages.tpl.php
@@ -0,0 +1,37 @@
+<?php
+
+$page_title = $node->title;
+// echo "<pre>";
+// print_r($node);
+// die();
+
+$header_image = $node->field_header_image_['und'][0]['uri'];
+$header_style = 'overview_header';
+$header_image_path = image_style_url($header_style, $header_image);
+
+$header_image_caption = $node->field_water_caption['und'][0]['value'];
+$page_body = $node->body['und'][0]['value'];
+
+// $body_image = $node->field_optional_image['und'][0]['uri'];
+// $body_style = 'program_image_410___320_';
+// $body_image_path = image_style_url($body_style,$body_image);
+
+?>
+<div class="economic-inner-main-wrapper">
+	<div class="detail-header-image"><img src="<?php echo $header_image_path; ?>">
+     <div class="haeder-caption"><?php print $header_image_caption; ?></div>
+    </div>
+
+	<div class="title-body-wrapper">
+	  <div class="economic-inner-title approach-title"><?php echo $page_title; ?></div>
+	  <div class="economic-inner-content approach-content-wrapper">
+<!-- 		  	<span class="shift-image">
+		  	<?php if($body_image){?>  <img src="<?php //echo $body_image_path; ?>"> <?php } ?>
+		  	</span> -->
+		  	<div class="approach-content">
+		  		<?php print $page_body; ?>
+		  	</div>
+		</div>
+	</div>
+
+</div>
