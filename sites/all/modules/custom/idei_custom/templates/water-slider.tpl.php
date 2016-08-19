c7d9ecdc0ca50ec5a8ec46550d04fd27978093db
Keshav <keshav.k@gai.co.in>
2016-08-19 17:23:01 +0530

    add next and prev button on water slider

--- sites/all/modules/custom/idei_custom/templates/water-slider.tpl.php
+++ sites/all/modules/custom/idei_custom/templates/water-slider.tpl.php
@@ -3,12 +3,12 @@
  print '<script src="'.$base_url.'/sites/all/modules/custom/idei_custom/js/jquery.waterwheelCarousel.js"></script>';?>
  <script type="text/javascript">
     $(document).ready(function () {
-      jQuery(".3d-content-wrap .3dd").hide();
-      jQuery(".3d-content-wrap .3dd.3d-item-0").show();
-      jQuery('.3d-check a').click(function() {
+      $(".3d-content-wrap .3dd").hide();
+      $(".3d-content-wrap .3dd.3d-item-0").show();
+      $('.3d-check a').click(function() {
         var a =jQuery(this).attr('class');
-          jQuery(".3d-content-wrap .3dd").fadeOut();
-          jQuery(".3d-content-wrap ."+a).fadeIn();
+          $(".3d-content-wrap .3dd").fadeOut();
+          $(".3d-content-wrap."+a).fadeIn();
       });
       var carousel = $("#carousel").waterwheelCarousel({
         flankingItems: 3,
@@ -16,20 +16,49 @@
           $('#callback-output').append('test');
         },
       });
+
       $('#reload').bind('click', function () {
         newOptions = eval("(" + $('#newoptions').val() + ")");
         carousel.reload(newOptions);
         return false;
       });
+
       $('#prev').bind('click', function () {
         carousel.prev();
         return false
       });
+
       $('#next').bind('click', function () {
         carousel.next();
         return false;
         });
+      
+      $('#prev').click(function() {      
+        setTimeout(function(){
+           $(".3d-check a").each(function() {
+            var slider_class = $(this).children().attr("class");
+            if(slider_class == 'carousel-center'){
+              var cls =$(this).attr("class");
+              $(".3d-content-wrap .3dd").fadeOut();
+              $(".3d-content-wrap .3dd."+cls).fadeIn();
+            }
+          });
+        }, 300);
       });
+
+      $('#next').click(function() {      
+        setTimeout(function(){
+           $(".3d-check a").each(function() {
+            var slider_class = $(this).children().attr("class");
+            if(slider_class == 'carousel-center'){
+              var cls =$(this).attr("class");
+              $(".3d-content-wrap .3dd").fadeOut();
+              $(".3d-content-wrap .3dd."+cls).fadeIn();
+            }
+          });
+        }, 300);
+      });  
+    });
   </script>
 
   <style type="text/css">
@@ -44,6 +73,7 @@
         cursor:pointer; 
       }
     </style>
+    
     <div class="navigation-button">
       <div class="prev-button"><a href="#" id="prev">Prev</a></div>
       <div class="next-button"> <a href="#" id="next">Next</a></div>
@@ -64,7 +94,7 @@ print '<div id="carousel" class="3d-check">';
     $body_style = 'program_image_410___320_';
     $body_image_path = image_style_url($body_style,$image);
 
-    print '<a href="#" class="3d-item-'.$i.'"><img src="'.$body_image_path.'" id="item-'.$i.'"  /></a>';
+    print '<a href="#" class="3d-item-'.$i.'"><img src="'.$body_image_path.'" id="item-'.$i.'" /></a>';
     }
     print '</div>';
     print '<div class="3d-content-wrap water-intro-content-wrap">';
