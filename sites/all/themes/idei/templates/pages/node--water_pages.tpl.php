5693e2f927b3faa67135c83e8d8e5a50de4a7017
vidushimehta <vidushi.2010@gmail.com>
2016-10-25 17:32:33 +0530

    css added for new pages of india fact and red flags

--- sites/all/themes/idei/templates/pages/node--water_pages.tpl.php
+++ sites/all/themes/idei/templates/pages/node--water_pages.tpl.php
@@ -1,6 +1,6 @@
 <div class="video-banner">
 	 <?php print views_embed_view('water_pages_','block_2'); ?>
 </div>
-<div class="field-collection-values">
+<div class="water-field-collection-values">
  <?php print views_embed_view('water_pages_','block_1'); ?>
 </div>
